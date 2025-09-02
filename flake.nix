6b40f17f116b3b9cf2323e9fcfa56630fb4eb9bb
Max Karou <maxkarou@protonmail.com>
2025-09-02 14:49:10 +0200

    feat(repo): initial structure

---
+++ flake.nix
@@ -0,0 +1,110 @@
+{
+  description = "<todo>";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        ## <todo>
+        ##
+        #@ String
+        name = "forge";
+
+        ## <todo>
+        ##
+        #@ Package
+        package = pkgs.emacs30;
+
+        ## <todo>
+        ##
+        #@ AttrSet
+        epkgs = pkgs.emacsPackagesFor package;
+
+        ## <todo>
+        ##
+        #@ [Package]
+        packages = with epkgs; [ evil ];
+
+        ## <todo>
+        ##
+        #@ AttrSet -> Derivation
+        mkEmacsPackage =
+          {
+            ## <todo>
+            ##
+            #@ Package
+            grammars ? epkgs.treesit-grammars.with-all-grammars,
+
+            ## <todo>
+            ##
+            #@ [Package]
+            extraPackages ? [ ],
+          }:
+          pkgs.symlinkJoin {
+            inherit name;
+
+            buildInputs = [ pkgs.makeWrapper ];
+
+            paths = [
+              (epkgs.emacsWithPackages (_: packages ++ extraPackages ++ [ grammars ]))
+            ];
+
+            postBuild = ''
+              # <todo>
+              # <todo>
+              $out/bin/emacs --batch \
+                --eval "(native-compile \"${self}/init.el\" \"$out/share/emacs/native-lisp/init.eln\")"
+
+              # <todo>
+              # <todo>
+              wrapProgram $out/bin/emacs \
+                --add-flags "--init-directory=\''${XDG_CONFIG_HOME:-\$HOME/.config}/emacs" \
+                --add-flags "--load $out/share/emacs/native-lisp/init.eln"
+            '';
+          };
+
+        ## <todo>
+        ##
+        #@ Derivation
+        forge = mkEmacsPackage { };
+      in
+      {
+        overlays.default = _: prev: {
+          ${name} = package;
+        };
+
+        # Shell used by "nix develop".
+        # See: https://nix.dev/manual/nix/2.18/command-ref/new-cli/nix3-develop
+        devShell = pkgs.mkShell {
+          shellHook = ''
+            echo "Entering the 'github:52/forge' development environment"
+            echo "Execute 'emacs' or 'emacs -nw' to open the build"
+          '';
+
+          packages =
+            (with pkgs; [
+              nixfmt-rfc-style
+              deadnix
+              statix
+              nixd
+            ])
+            ++ [ forge ];
+        };
+      }
+    );
+}
---
+++ init.el
@@ -0,0 +1,37 @@
+;;; init.el -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2025 Max Karou
+;;
+;; Author: Max Karou <maxkarou@protonmail.com>
+;; URL: https://github.com/52/<todo>
+;;
+;; Licensed under MIT License, or Apache Version 2.0, at your discretion.
+;;
+;; MIT License: http://opensource.org/licenses/MIT
+;; Apache Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+;;
+;; Usage of this file is permitted solely under a sanctioned license.
+
+;;; Commentary:
+
+;; <todo>
+;; <todo>
+;; <todo>
+;; <todo>
+
+;;; Code:
+
+;; <todo>
+(when (< emacs-major-version 30)
+  (error "This configuration only works with Emacs 30 and newer; you have version %s"
+         emacs-major-version))
+
+;; <todo>
+(require 'use-package)
+
+;; <todo>
+(message "<todo> loaded successfully!")
+
+(provide 'init)
+
+;;; init.el ends here
