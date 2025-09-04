a44d59cfa25ccd2283ce8d6fb29ee54c22b059ce
Max Karou <maxkarou@protonmail.com>
2025-09-04 12:50:13 +0200

    feat(theme): added `hades-theme.el`

---
+++ themes/hades-theme.el
@@ -0,0 +1,41 @@
+;;; hades-theme.el -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2025 Max Karou
+;;
+;; Author: Max Karou <maxkarou@protonmail.com>
+;; URL: https://github.com/52/forge
+;;
+;; Licensed under MIT License, or Apache Version 2.0, at your discretion.
+;;
+;; MIT License: http://opensource.org/licenses/MIT
+;; Apache Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+;;
+;; Usage of this file is permitted solely under a sanctioned license.
+
+;;; Commentary:
+
+;;; Code:
+
+(deftheme hades
+  "A dark color scheme fitting for the king of the underworld.")
+
+(let* ((class '((class color) (min-colors 256)))
+
+       (font-family "monospace")
+       (font-weight 'Light)
+       (font-height '140))
+
+  (custom-theme-set-faces
+   'hades
+
+   ))
+
+;;;###autoload
+(when (and (boundp 'custom-theme-load-path) load-file-name)
+  (add-to-list 'custom-theme-load-path
+               (file-name-as-directory (file-name-directory load-file-name))))
+
+(provide-theme 'hades)
+(provide 'hades-theme)
+
+;; hades-theme.el ends here
