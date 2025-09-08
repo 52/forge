41a8a4599bc0ea00f9861c989afaf75e91a21b18
Max Karou <maxkarou@protonmail.com>
2025-09-08 23:07:23 +0200

    feat(+evil.el): added `evil` package

--- modules/+evil.el
+++ modules/+evil.el
@@ -18,8 +18,38 @@
 ;;; Code:
 
 (use-package evil
+  :preface
+  (defun forge--adjust-scroll-margin ()
+    "Adjust the `scroll-margin' near end-of-buffer.
+This prevents extra lines at EOB when the point is within that distance."
+    (unless (or (minibufferp) (window-minibuffer-p))
+      (let* ((n (default-value 'scroll-margin))
+             (new (if (and (> n 0) (save-excursion (forward-line n) (eobp))) 0 n)))
+        (unless (eq scroll-margin new)
+          (setq-local scroll-margin new)))))
+
+  (defun forge--adjust-shift-width ()
+    "Adjust `evil-shift-width' to mirror `tab-width'.
+This keeps the indentation operators (>> and <<) aligned across major modes."
+    (unless (derived-mode-p 'org-mode)
+      (setq-local evil-shift-width tab-width)))
+  :init
+  ;; Prefer `undo-fu' as the default undo-backend.
+  (setq evil-undo-system 'undo-fu)
+
+  ;; Prefer a uniform cursor shape across motions.
+  (setq evil-normal-state-cursor 'box)
+  (setq evil-insert-state-cursor 'box)
+  (setq evil-visual-state-cursor 'box)
   :config
-  (evil-mode 1))
+  ;; Adjust `scroll-margin' near end-of-buffer.
+  (add-hook 'post-command-hook #'forge--adjust-scroll-margin)
+
+  ;; Keep `evil-shift-width' consistent with `tab-width'.
+  (add-hook 'after-change-major-mode-hook #'forge--adjust-shift-width)
+
+  ;; Enable `evil-mode' after initialization.
+  (add-hook 'after-init-hook #'evil-mode))
 
 (provide '+evil)
 
