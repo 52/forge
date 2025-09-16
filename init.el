03ed8c89919e524748bcbce19101cd9f1bdfe03d
Max Karou <maxkarou@protonmail.com>
2025-09-16 18:13:32 +0200

    feat(init): modify load-order

--- init.el
+++ init.el
@@ -22,7 +22,7 @@
     ;; Core/Foundation modules.
     ;; These define the base editing environment and interface.
     ;; See: https://github.com/52/forge/tree/master/core
-    (@editor @ui @completion @tty @treesit @prog @evil @macro)
+    (@macro @editor @evil @ui @completion @treesit @prog @tty)
     ;;
     ;; Language-specific modules.
     ;; These define programming language support and developer tools.
